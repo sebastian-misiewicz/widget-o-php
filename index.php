77fec5058f25a9d6795a081cf22cbd6017668aaa
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-07-09 19:03:39 +0200

    Added dibi depencency

--- index.php
+++ index.php
@@ -4,7 +4,23 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 $app->get('/:name+', function ($name) {
-    echo file_get_contents("./templates/index.html");
+    
+    dibi::connect(array(
+        'driver'   => 'mysql',
+        'host'     => 'localhost',
+        'username' => 'root',
+        'password' => 'abcd',
+        'database' => 'widget-o'
+    ));
+    
+    $idsite = implode('/', $name);
+    
+    $result = dibi::query('select idsite, template, json FROM `site` where idsite = %s', $idsite);
+    
+    // TODO 404 if not found
+    $site = $result->fetchAll()[0];
+    
+    echo str_replace('{page:"page"}', $site->json, file_get_contents("templates/".$site->template));
 });
 $app->run();
 
