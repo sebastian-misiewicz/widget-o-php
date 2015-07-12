cf9b91a018c506a10240959a8aa28d4560b5d470
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-07-12 11:42:56 +0200

    Authorization and composer config update

---
+++ Widgeto/Middleware/Authorization.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Widgeto\Middleware;
+
+class Authorization extends \Slim\Middleware {    
+
+    protected $headers = array();
+    
+    public function __construct($headers) {
+        $this->headers = $headers;
+    }
+    
+    public function call() {
+        // TODO finish it up
+        $this->next->call();
+    }
+
+}
+
---
+++ Widgeto/Service/UserService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Widgeto\Service;
+
+class UserService {
+    
+    public static function check($username, $password) {
+        $result = dibi::query(
+                'select username FROM `user` where username = %s and password = %s', 
+                $username, $password);
+        
+        return $result->count() == 0;
+    }
+    
+}
+
