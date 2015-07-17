0249579cd6eb91b77d9f302cf6291c2a1c662dec
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-07-17 19:59:29 +0200

    Introduced StringService

--- Widgeto/Middleware/Authorization.php
+++ Widgeto/Middleware/Authorization.php
@@ -3,6 +3,7 @@
 namespace Widgeto\Middleware;
 
 use Widgeto\Service\UserService;
+use Widgeto\Service\StringService;
 
 class Authorization extends \Slim\Middleware {    
 
@@ -14,7 +15,7 @@ class Authorization extends \Slim\Middleware {
     
     public function call() {
         $app = $this->getApplication();
-        if ($this->startsWith($app->request->getPathInfo(), "/rest/") ) {
+        if (StringService::startsWith($app->request->getPathInfo(), "/rest/") ) {
             if (!isset($this->headers["auth-token"])) {
                 return $this->status403();
             }
@@ -34,10 +35,5 @@ class Authorization extends \Slim\Middleware {
         $this->next->call();
     }
     
-    // Thanks: http://stackoverflow.com/a/10473026
-    function startsWith($haystack, $needle) {
-        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
-    }
-
 }
 
