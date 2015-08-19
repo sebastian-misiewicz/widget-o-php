ded1adf028ddb525c8709a2ec89c4dfb00cf9c68
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-08-19 17:33:21 +0200

    Added editing mode

--- Widgeto/Rest/HomeRest.php
+++ Widgeto/Rest/HomeRest.php
@@ -8,18 +8,34 @@ class HomeRest {
 
     /* @var $app \Slim\Slim */
     public function __construct($app) {
-        $app->get('/:name+', function ($name) {
+        $app->get('/edit-:name+', function ($name) use ($app) {
             $page = PageService::getPage($name);
-
+            
+            if ($page == NULL) {
+                $app->notFound();
+            }
+            
             echo str_replace(
                     array('{idpage}', '{page:"page"}'), array($page->idpage, $page->json), file_get_contents("templates/" . $page->template));
         });
+        
+        $app->get('/:name+', function ($name) use ($app) {
+            $page = PageService::getPage($name);
+
+            if ($page == NULL) {
+                $app->notFound();
+            }
+            
+            echo str_replace(
+                    array('{idpage}', '{page:"page"}'), array($page->idpage, $page->json), file_get_contents("rendered/" . $page->template));
+        });
+        
         
         $app->get('/', function () {
             $page = PageService::getPage(array('index.html'));
 
             echo str_replace(
-                    array('{idpage}'), array($page->idpage), file_get_contents("templates/" . $page->template));
+                    array('{idpage}'), array($page->idpage), file_get_contents("rendered/" . $page->template));
         });
     }
     
