461a98f2b129bc51888b8a4d138a4b59b3465765
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-09-03 21:43:31 +0200

    Edit on parameter "edit" instead of changed page id

--- Widgeto/Rest/HomeRest.php
+++ Widgeto/Rest/HomeRest.php
@@ -8,28 +8,21 @@ class HomeRest {
 
     /* @var $app \Slim\Slim */
     public function __construct($app) {
-        $app->get('/edit-:name+', function ($name) use ($app) {
-            $page = PageService::getPage($name);
-            
-            if ($page == NULL) {
-                $app->notFound();
-            }
-            
-            echo str_replace(
-                    array('{idpage}', '{page:"page"}'), array($page->idpage, $page->json), file_get_contents("templates/" . $page->template));
-        });
-        
         $app->get('/:name+', function ($name) use ($app) {
             $page = PageService::getPage($name);
-
+            
             if ($page == NULL) {
                 $app->notFound();
             }
             
+            $source = "rendered";
+            if ($app->request->get('edit') == true) {
+                $source = "templates";
+            }
+            
             echo str_replace(
-                    array('{idpage}', '{page:"page"}'), array($page->idpage, $page->json), file_get_contents("rendered/" . $page->template));
+                    array('{idpage}', '{page:"page"}'), array($page->idpage, $page->json), file_get_contents($source . "/" . $page->template));
         });
-        
         
         $app->get('/', function () use ($app) {
             $page = PageService::getPage(array('index.html'));
