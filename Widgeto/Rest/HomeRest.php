691d43510738730bb3a18a61364d1aeccc06134c
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-09-05 22:32:20 +0200

    Code style correction

--- Widgeto/Rest/HomeRest.php
+++ Widgeto/Rest/HomeRest.php
@@ -8,32 +8,30 @@ class HomeRest {
 
     /* @var $app \Slim\Slim */
     public function __construct($app) {
-        $app->get('/:name+', function ($name) use ($app) {
-            $page = PageService::getPage($name);
-            
-            if ($page == NULL) {
-                $app->notFound();
-            }
-            
-            $source = "rendered";
-            if ($app->request->get('edit') == true) {
-                $source = "templates";
-            }
-            
-            echo str_replace(
-                    array('{idpage}', '{page:"page"}'), array($page->idpage, $page->json), file_get_contents($source . "/" . $page->template));
+        $parent = $this;
+        $app->get('/:name+', function ($name) use ($app, $parent) {
+            $parent->getPage($app, $name);
         });
         
-        $app->get('/', function () use ($app) {
-            $page = PageService::getPage(array('index.html'));
-
-            if ($page == NULL) {
-                $app->notFound();
-            }
-            
-            echo str_replace(
-                    array('{idpage}'), array($page->idpage), file_get_contents("rendered/" . $page->template));
+        $app->get('/', function () use ($app, $parent) {
+            $parent->getPage($app, array('index.html'));
         });
+    }
+    
+    function getPage($app, $name)  {
+        $page = PageService::getPage($name);
+            
+        if ($page == NULL) {
+            $app->notFound();
+        }
+
+        $source = "rendered";
+        if ($app->request->get('edit') == true) {
+            $source = "templates";
+        }
+
+        echo str_replace(
+                array('{idpage}', '{page:"page"}'), array($page->idpage, $page->json), file_get_contents($source . "/" . $page->template));
     }
     
 }
