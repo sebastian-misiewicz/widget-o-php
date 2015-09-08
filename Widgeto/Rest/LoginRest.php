c14642411ffedbc857ac0eecdcdbfcbdb75510d9
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-09-08 18:07:58 +0200

    Corrected database communication during login

--- Widgeto/Rest/LoginRest.php
+++ Widgeto/Rest/LoginRest.php
@@ -8,7 +8,7 @@ class LoginRest {
     public function __construct($app) {
         $app->post('/rest/login/', function () use ($app) {
             $user = json_decode($app->request->getBody(), true);
-            
+
             if (!isset($user['username']) || empty($user['username'])) {
                 $app->error();
             }
@@ -19,23 +19,23 @@ class LoginRest {
             
             $result = \dibi::query(
                 'select username FROM `user` where username = %s and password = %s', 
-                $user['username'], md5($user['password']));
+                $user['username'], md5($user['password']))
+                    ->fetchAll();
             
-            if (!$result) {
-                return $this->error();
+            if (sizeof($result) != 1) {
+                return $app->error();
             }
             
             $oldToken = \dibi::query(
                     'select token from `oauth` where username = %s', 
-                    $user['username']);
+                    $user['username'])->fetch();
             
             $token = bin2hex(openssl_random_pseudo_bytes(16));
             
             $auth = array();
             $auth["username"] = $user['username'];
             $auth["token"] = $token;
-            
-            if ($oldToken) {
+            if ($oldToken["token"]) {
                 \dibi::query(
                         'update `oauth` set token = %s where username = %s', 
                         $token, $user['username']);
