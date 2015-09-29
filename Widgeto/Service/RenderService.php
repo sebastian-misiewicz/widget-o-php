36ecd5a39b49c51fffe0de40e6309c55ab0725bd
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2015-09-29 19:56:52 +0200

    Corrected bug with replacing ng-*

--- Widgeto/Service/RenderService.php
+++ Widgeto/Service/RenderService.php
@@ -10,8 +10,8 @@ class RenderService {
                 $html = '<!DOCTYPE html><html lang="en">' . $html;
             }
             
-            $html = preg_replace('/ng-[a-z\-]+=\"[^\"]+\"/i', "", $html);
-            $html = preg_replace('/ng-[a-z]+/i', "", $html);
+            $html = preg_replace('/([\" ])ng-[a-z\-]+=\"[^\"]+\"/i', "$1", $html);
+            $html = preg_replace('/([\" ])ng-[a-z]+/i', "$1", $html);
             $html = preg_replace('/[a-z]+=\"[ ]*\"/i', "", $html);
             $html = preg_replace('/<!-- widget-o:no-render.+/si', "", $html);
             $html = $html . "</body></html>";
